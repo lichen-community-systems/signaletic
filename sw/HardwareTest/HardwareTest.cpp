195e101232a3c301f3ba0f09476e7b22040d5a12
Colin Clark <colinbdclark@gmail.com>
2022-12-03 17:07:04 -0500

    Squashed 'hosts/daisy/vendor/dpt/' changes from be37bdfd..ef00d7d4
    ef00d7d4 removed extra inversion in WriteCvOutExp
    b2e7b2ea Folded in ReverbSc for audio testing.
    11836f0c added some comments and a boundary check
    bd0acab7 incorrectly committed an incomplete main file, fixing
    ff19270d Straight port of patch_sm reverb example.
    3a1f74a2 Merge branch 'main' of https://github.com/joemisra/dpt
    166111e2 Updates for MIDI Polysynth Example "MegaBasic"
    654e7a90 Update README.md
    2382d343 Had accidentally left usb MIDI attempt in, this did not work yet
    5364c8d7 Initial import for template project.
    0beae489 Merge branch 'main' of https://github.com/joemisra/dpt
    d760236d example updates
    6898337e updated to use DMA calls w/ SPI
    42c8b949 Merge branch 'main' of https://github.com/joemisra/dpt
    2b665811 spi / timer stuff
    9d0fb71e Update README.md
    5344df96 Merge commit '7512a192271fb3a590bb07f83f7389704df0d3cc'
    7512a192 attempting again to update libdaisy to current ver
    f46fcc66 attempting to fix libdaisy submodule ref
    7e09ed4b Update README.md
    23298fc6 added some gpio setup for 2 external buttons, uses usb/midi pins
    6286255a various tweaks
    a10260b3 changes to use libdaisy 5.0 timer code
    e3140118 various tweaks
    fa2240bd Merge branch 'main' of https://github.com/joemisra/dpt
    80b9e136 Update README.md
    2cb7234b Update README.md
    40cdf541 Merge branch 'main' of https://github.com/joemisra/dpt
    7e040db3 importing megabasic midi polysynth
    be03d505 add gitignore
    e994a3db some refactoring, separated out voice code
    d3ba5194 Update README.md
    d38a4b4f Create README.md
    7b96a8d2 comments
    a21c03d3 added support for raw output, some comments
    c490853a Merge branch 'main' of https://github.com/joemisra/dpt
    c7cb2847 adding basic i2c leader example
    8e34baa0 Update README.md
    033614e4 removed extra old libdaisy directory
    1b20a301 Merge branch 'main' of https://github.com/joemisra/dpt
    c0ea7fb9 restructuring project, libdaisy + daisysp as submodules, beginning HardwareTest mods for DPT
    fbcdd306 Update README.md
    5dbd19c2 Update README.md
    bd7c0b7a Update README.md
    337c971c Update README.md
    45251f7b Update README.md
    f4c5ec4a Update README.md
    d2f9e2bd Update README.md
    c45f00dd Update README.md
    3a817973 Update README.md
    90a21ff7 Update README.md
    b1531320 Update README.md
    5e78e0d0 Update README.md
    07ba707b Update README.md
    2ce2b849 Update README.md
    b3526ed2 Update README.md
    46e17b28 Update README.md
    92bc25e3 Update README.md
    5c59d486 Update README.md
    45d4c6ce Update README.md
    ad1ad63b Update README.md
    dc85a12c Update README.md
    d4c06c8a Added BOM
    d18c16f4 Update README.md
    0b03b4a5 Create README.md
    5df67fcb Update README.md
    8425aac7 Add files via upload
    aea4a939 Uploading initial work files
    8962d951 Initial commit
    REVERT: be37bdfd gh-22: Moves Bluemchen and DPT libraries out of the examples dir.
    
    git-subtree-dir: hosts/daisy/vendor/dpt
    git-subtree-split: ef00d7d45a1fdfe0060c10387a177e3efdbe2c8c

--- sw/HardwareTest/HardwareTest.cpp
+++ sw/HardwareTest/HardwareTest.cpp
@@ -11,6 +11,9 @@ DPT hw;
 Switch         button, toggle;
 FIL            file; /**< Can't be made on the stack (DTCMRAM) */
 FatFSInterface fsi;
+Oscillator osc[6];
+ReverbSc reverb;
+CrossFade cf;
 
 using MyOledDisplay = OledDisplay<SSD130xI2c128x32Driver>;
 MyOledDisplay         display;
@@ -29,15 +32,47 @@ void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
                    size_t                    size)
 {
+
     hw.ProcessAllControls();
-    button.Debounce();
-    toggle.Debounce();
+    /*
+        CV 1    Oscillator Frequency
+        CV 2    Reverb Feedback (0 - 99%)
+        CV 3    Reverb Damping (0 - 24000hz)
+        CV 4    Wet/Dry Mix
+    */
+
+    for(int i=0; i<6; i++) {
+        osc[i].SetFreq(hw.controls[CV_1].Value() * 99.f);
+    }
+
+    reverb.SetFeedback(abs(hw.controls[CV_2].Value()));
+    reverb.SetLpFreq(abs(hw.controls[CV_3].Value()) * 24000.f);
+
+    cf.SetPos(abs(hw.controls[CV_4].Value()));
+
     for(size_t i = 0; i < size; i++)
     {
-        out[0][i] = in[0][i];
-        out[1][i] = in[1][i];
+        float wetl, wetr, dryl, dryr;
+
+        dryl = IN_L[i];
+        dryr = IN_R[i];
+        
+        /* Fill audio buffer here, please */
+        reverb.Process(dryl, dryr, &wetl, &wetr);
+
+        OUT_L[i] = cf.Process(dryl, wetl);
+        OUT_R[i] = cf.Process(dryr, wetr);
     }
+
+    /* These methods want 0-4095 if that last 'raw' parameter is true,
+       so the amplitude of all of these oscillators is set to 2048.f, and this is bipolar.
+       So we add 2048.f to bring it up.
+       */
+    hw.WriteCvOutExp(osc[0].Process() + 2048.f, osc[1].Process() + 2048.f, osc[2].Process() + 2048.f, osc[3].Process() + 2048.f, true);
+    hw.WriteCvOut(1, osc[4].Process() + 2048.f, true);
+    hw.WriteCvOut(2, osc[5].Process() + 2048.f, true);
 }
+
 void MIDISendNoteOn(uint8_t channel, uint8_t note, uint8_t velocity)
 {
     uint8_t data[3] = {0};
@@ -62,25 +97,81 @@ void MIDISendNoteOff(uint8_t channel, uint8_t note, uint8_t velocity)
 
 int main(void)
 {
+    float samplerate = 48000;
     hw.Init();
+    hw.SetAudioSampleRate(samplerate);
+    hw.SetAudioBlockSize(48);
+
+    /* Set up some basic oscillators to write to the internal/external 12-bit DACs */
+
+    for(int i=0;i<6;i++) {
+        osc[i].Init(samplerate);
+        osc[i].SetWaveform(Oscillator::WAVE_SIN);
+        osc[i].SetAmp(2048.f);
+        osc[i].Reset(i * (1/6.f));
+        osc[i].SetFreq(10.f);
+    }
+
+    reverb.Init(samplerate);
+
+    cf.Init();
+
+    SdmmcHandler::Config sd_config;
+    SdmmcHandler         sdcard;
+    sd_config.Defaults();
+    sdcard.Init(sd_config);
+
+    fsi.Init(FatFSInterface::Config::MEDIA_SD);
+
+    /** Write/Read text file */
+    const char *test_string = "Testing Daisy Patch SM";
+    const char *test_fname  = "DaisyPatchSM-Test.txt";
+    FRESULT     fres = FR_DENIED; /**< Unlikely to actually experience this */
+    if(f_mount(&fsi.GetSDFileSystem(), "/", 0) == FR_OK)
+    {
+        /** Write Test */
+        if(f_open(&file, test_fname, (FA_CREATE_ALWAYS | FA_WRITE)) == FR_OK)
+        {
+            UINT   bw  = 0;
+            size_t len = strlen(test_string);
+            fres       = f_write(&file, test_string, len, &bw);
+        }
+        f_close(&file);
+        if(fres == FR_OK)
+        {
+            /** Read Test only if Write passed */
+            if(f_open(&file, test_fname, (FA_OPEN_EXISTING | FA_READ)) == FR_OK)
+            {
+                UINT   br = 0;
+                char   readbuff[32];
+                size_t len = strlen(test_string);
+                fres       = f_read(&file, readbuff, len, &br);
+            }
+            f_close(&file);
+        }
+    }
+
+    bool sdmmc_pass = fres == FR_OK;
 
     //hw.midi.StartReceive();
     //hw.StartLog(false);
 
     //hw.usb.Init(UsbHandle::UsbPeriph::FS_EXTERNAL);
 
-    uint32_t now, dact, usbt, gatet;
+    uint32_t now, dact, usbt;
     now = dact = usbt = System::GetNow();
-    gatet             = now;
 
-    //hw.usb_midi.StartReceive();
+    hw.midi.StartReceive();
+
+    hw.StartAudio(AudioCallback);
 
     while(1)
     {
+        /* This loop will listen for MIDI and trigger gates for feedback */
         now = System::GetNow();
-        hw.usb_midi.Listen();
+        hw.midi.Listen();
 
-        if(hw.usb_midi.HasEvents()) {
+        if(hw.midi.HasEvents()) {
             auto event = hw.midi.PopEvent();
 
             if(event.type  == MidiMessageType::NoteOn) {
@@ -89,11 +180,14 @@ int main(void)
             else if(event.type  == MidiMessageType::NoteOff) {
                 dsy_gpio_write(&hw.gate_out_1, 0);
             }
+            /*
             else if(event.type == MidiMessageType::ControlChange) {
                 ControlChangeEvent e = event.AsControlChange();
                 hw.WriteCvOut(CV_OUT_2, ((float)e.value / 127.) * 5.f, false);
             }
+            */
         } else {
+            dsy_gpio_write(&hw.gate_out_1, 0);
             dsy_gpio_write(&hw.gate_out_2, 0);
         }
         hw.Delay(50);
